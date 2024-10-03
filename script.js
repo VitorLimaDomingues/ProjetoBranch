684ebf58afeda3ab216b116e6338f748c86ab58c
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2024-10-03 11:33:06 -0300

    Criando a array, objeto e metodo

--- script.js
+++ script.js
@@ -28,8 +28,27 @@ while (w<5);
 function Ola(nome){
     // `` (Strings literal ou template string)
     console.log(`Seja bem-vindo a ${nome}`)
-    alert(`Seja bem-vindo a ${nome}`)
+    // alert(`Seja bem-vindo a ${nome}`)
     document.write(`Seja bem-vindo a ${nome}`)
 }
 // Executando a função Ola passando o valor
 Ola('FIAP')
+
+// Arrow Function
+
+const hello=()=>(console.log("Função arrow function"))
+
+hello()
+
+// array [], objeto {} e metodos()
+
+let jogadores =[                  // array
+    {nome:'Huguinho', idade:17},  // objeto
+    {nome:'Zezinho', idade:18},   // objeto
+    {nome:'Luizinho', idade:19}   // objeto
+]
+
+let listarjogadores = jogadores.map(function(item){
+    return item.idade             // metodos
+})
+console.log(listarjogadores)
