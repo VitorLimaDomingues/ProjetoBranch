ec649b9767c55378c9625754cc3ed9fa52145c28
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2024-10-03 11:24:10 -0300

    Criando função em javascript

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // Estrutura de repetição
 
 for (let i=0; i<10;i++){
-    console.log(i)
+    // console.log(i)
 }
 
 // While
 
 let f =0;
 while (f <10){
-    console.log(f)
+    // console.log(f)
     f++;    
 }
+
+// do while
+
+let w=0;
+
+do{
+    // console.log(w);
+    w++;
+
+}
+while (w<5);
+
+// Funções
+
+function Ola(nome){
+    // `` (Strings literal ou template string)
+    console.log(`Seja bem-vindo a ${nome}`)
+    alert(`Seja bem-vindo a ${nome}`)
+    document.write(`Seja bem-vindo a ${nome}`)
+}
+// Executando a função Ola passando o valor
+Ola('FIAP')
